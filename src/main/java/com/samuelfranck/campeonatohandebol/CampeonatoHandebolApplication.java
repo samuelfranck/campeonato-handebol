ce009e2fb6c16e08008309a2246d72f65a980b0d
Samuel Franck <samuelfranck@gmail.com>
2020-05-04 11:04:32 -0300

    Projeto Campeonato de Handebol criado

---
+++ src/main/java/com/samuelfranck/campeonatohandebol/CampeonatoHandebolApplication.java
@@ -0,0 +1,13 @@
+package com.samuelfranck.campeonatohandebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampeonatoHandebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampeonatoHandebolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelfranck/campeonatohandebol/CampeonatoHandebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelfranck.campeonatohandebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampeonatoHandebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
